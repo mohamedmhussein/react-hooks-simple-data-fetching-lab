74da1998066a600ed935c3228b323db600578555
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-06-15 22:28:34 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import react, { useState, useEffect } from "react";
+
+function App() {
+
+
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setImage(data.message)
+            })
+
+    }, [])
+    if (!image) { return <p>Loading...</p> }
+
+    return (
+
+        <img src={image} alt="A Random Dog" />
+    )
+
+}
+
+export default App
